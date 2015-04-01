330b4691c8a3f361b9ff9d6213b55c84fbec90eb
Peter Cardi <peter.v.cardi@gmail.com>
2015-04-01 10:04:03 -0600

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  helper_method :fnln
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: "User was successfully created"
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    user_params = params.require(:user).permit(:first_name, :middle_name, :last_name, :email, :about_me)
+    @user.update(user_params)
+    redirect_to users_path, notice: "User was successfully updated"
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, notice: "User was successfully deleted"
+  end
+
+  def fnln
+    "#{user.first_name} #{user.last_name}"
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :middle_name, :last_name, :email, :about_me)
+    end
+
+end
